36747a510143310eacbf95c62b9c025c6adfd842
‘yufengjiaoyu’ <1147305508@qq.com>
2021-10-22 09:13:04 +0800

    完成用户增删改查

--- src/components/admin/UserList.vue
+++ src/components/admin/UserList.vue
@@ -8,8 +8,17 @@
     <el-card class="box-card">
       <el-row :gutter="25">
         <el-col :span="10">
-          <el-input placeholder="请输入搜索内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input
+            placeholder="请输入搜索内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getUserList"
+          >
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="getUserList"
+            ></el-button>
           </el-input>
         </el-col>
 
@@ -30,7 +39,10 @@
         <el-table-column label="状态" prop="state">
           <!-- 作用域插槽 -->
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.state"></el-switch>
+            <el-switch
+              v-model="scope.row.state"
+              @change="userStateChanged(scope.row)"
+            ></el-switch>
           </template>
         </el-table-column>
 
@@ -63,22 +75,78 @@
                 size="mini"
               ></el-button>
             </el-tooltip>
-          </template> 
+          </template>
         </el-table-column>
       </el-table>
 
-     <!-- 分页 -->
+      <!-- 分页 -->
       <el-pagination
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
-        :current-page= "queryInfo.pageNum"
+        :current-page="queryInfo.pageNum"
         :page-sizes="[1, 2, 5, 10]"
-        :page-size= "queryInfo.pageSize"
+        :page-size="queryInfo.pageSize"
         layout="total, sizes, prev, pager, next, jumper"
         :total="total"
       ></el-pagination>
-
     </el-card>
+
+    <!-- 添加用户对话框 -->
+    <el-dialog
+      title="添加用户"
+      :visible.sync="addDialogVisible"
+      width="50%"
+      @close="addDialogClosed"
+    >
+      <!-- 内容主体区域 -->
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="70px"
+      >
+        <!-- 用户名 -->
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <!-- 邮箱 -->
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 内容底部区域 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </span>
+    </el-dialog>
+
+<!-- 修改用户对话框 -->
+    <el-dialog title="修改用户信息" :visible.sync="editDialogVisible" width="50%" @colse="editDialogClosed">
+       <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="70px">
+        <!-- 用户名 -->
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="editForm.username" disabled></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="editForm.password"></el-input>
+        </el-form-item>
+        <!-- 邮箱 -->
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="editForm.email"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editUserInfo">确 定</el-button>
+      </span>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -96,32 +164,150 @@ export default {
       },
       userList: [], //用户列表
       total: 7, // 总记录数
+      addDialogVisible: false ,// 对话框显示
+      // 添加用户表单项
+      addForm: {
+        username:'',
+        password:'',
+        email:'',
+      }, 
+      addFormRules:{
+        username:[
+           { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 5, max: 8, message: "长度在 5 到 8 个字符", trigger: "blur" }
+        ],
+        password:[
+           { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 8, message: "长度在 6 到 8 个字符", trigger: "blur" }
+        ],
+        email:[
+           { required: true, message: "请输入邮箱", trigger: "blur" },
+          { min: 5, max: 15, message: "请输入正确邮箱地址", trigger: "blur" }
+        ],
+      },
+       // 控制修改用户对话框显示/隐藏
+      editDialogVisible:false,
+      // 修改用户信息
+      editForm:{},
+         // 修改用户表单验证规则
+      editFormRules:{
+          password:[
+           { required: true, message: "请输入密码", trigger: "blur" },
+           { min: 6, max: 8, message: "长度在 6 到 8 个字符", trigger: "blur" }
+          ],
+          email:[
+           { required: true, message: "请输入邮箱", trigger: "blur" },
+           { min: 5, max: 15, message: "请输入正确邮箱地址", trigger: "blur" }
+          ],
+      },
     };
   },
   methods: {
-     async getUserList() {
+    // 修改用户状态
+    async userStateChanged(userinfo) {
+      const { data: res } = await this.$http.put(
+        `userState?id=${userinfo.id}&state=${userinfo.state}`
+      );
+      if (res != "success") {
+        userinfo.state = !userinfo.state;
+        return this.$message.error("操作失败！！！");
+      }
+      this.$message.success("操作成功！！！");
+    },
+ // 展示修改框
+    async showEditDialog(id){
+       
+        const {data:res} = await this.$http.get("getupdate?id="+id);
+        // if (res != "success") {
+        // userinfo.state = !userinfo.state;
+        // return this.$message.error("操作失败！！！");
+        // }
+        // this.$message.success("操作成功！！！");
+
+        this.editForm = res;
+        this.editDialogVisible = true;
+    },
+    // 关闭窗口
+    editDialogClosed(){
+      this.$refs.editFormRef.resetFields();
+    },
+    // 确认修改
+    editUserInfo(){
+      this.$refs.editFormRef.validate(async valid =>{
+        console.log(valid);
+        if( !valid ) return;
+        // 发起请求
+        const {data:res} = await this.$http.put("edituser",this.editForm);
+        console.log(res);
+         if (res != "success") return this.$message.error("操作失败！！！");
+        this.$message.success("操作成功！！！");
+        //隐藏
+        this.editDialogVisible = false;
+        this.getUserList();
+      });
+    },
+
+    async deleteUser(id){
+   const confirmResult = await  this.$confirm('是否删除用户？','提示',{
+        confirmButtonText:'确定',
+        cancelButtonText:'取消',
+        type:'warnig'
+      }).catch(err => err)
+      if(confirmResult != 'confirm'){
+      return  this.$message.info("已取消删除");
+      }
+    const {data: res}= await this.$http.get("deleteuser?id="+id);
+    if(res != "success"){
+      return this.$message.error("删除失败")
+    }
+      return this.$message.success("删除成功");
+      this.getUserList();
+    },
+    addDialogClosed(){
+      this.$refs.addFormRef.resetFields();
+    },
+    addUser(){
+      this.$refs.addFormRef.validate(async valid =>{
+        if(!valid) return;
+   const {data: res} = await this.$http.post("adduser",this.addForm)
+   if(res!="success"){
+     return this.$message.error("操作失败");
+   }else {
+      this.$message.success("操作成功");
+      this.addDialogVisible= false;
+      this.getUserList();
+   }
+      })
+    },
+    async userStateChanged(userInfo) {
+      const { data: res } = await this.$http.put(
+        `updatestate?id=${userInfo.id}&state=${userInfo.state}`
+      );
+      if (res != "success") {
+        userInfo.id = !userInfo.id;
+        return this.$message.error("操作失败!!!");
+      }
+      this.$message.success("操作成功");
+    },
+    async getUserList() {
       const { data: res } = await this.$http.get("alluser", {
         params: this.queryInfo,
       });
-     
+
       this.userList = res.data;
       this.total = res.numbers;
-      
     },
-    handleSizeChange(newSize){
+    handleSizeChange(newSize) {
       this.queryInfo.pageSize = newSize;
-     this.queryInfo.pageNum=1;
+      this.queryInfo.pageNum = 1;
       this.getUserList();
-
     },
 
-    handleCurrentChange(newPage){
-     this.queryInfo.pageNum = newPage;
-      
+    handleCurrentChange(newPage) {
+      this.queryInfo.pageNum = newPage;
+
       this.getUserList();
-   
     },
-   
   },
 };
 </script>
